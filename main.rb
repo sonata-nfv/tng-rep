bd6622a53819ac281bcf261624e2714c79a6b714
Felipe Vicens <felipe.vicens@atos.net>
2019-04-27 21:55:37 +0200

    Cleaning main.rb
    Signed-off-by: Felipe Vicens <felipe.vicens@atos.net>

--- main.rb
+++ main.rb
@@ -32,7 +32,7 @@
 
 # Set environment
 ENV['RACK_ENV'] ||= 'production'
-ENV['SEC_FLAG'] ||= true
+#ENV['SEC_FLAG'] ||= true
 
 require 'sinatra'
 require 'sinatra/config_file'
@@ -51,98 +51,97 @@ require_relative 'helpers/init'
 
 configure do
   # Configuration for logging
-  enable :logging
-  Dir.mkdir("#{settings.root}/log") unless File.exist?("#{settings.root}/log")
-  log_file = File.new("#{settings.root}/log/#{settings.environment}.log", 'a+')
-  log_file.sync = true
-  use Rack::CommonLogger, log_file
-
-  LOGGER = LOGGER.new(log_file)
-  LOGGER.level = LOGGER::DEBUG
-  set :LOGGER, LOGGER
+  # enable :logging
+  # Dir.mkdir("#{settings.root}/log") unless File.exist?("#{settings.root}/log")
+  # log_file = File.new("#{settings.root}/log/#{settings.environment}.log", 'a+')
+  # log_file.sync = true
+  # use Rack::CommonLogger, log_file
+  # LOGGER = LOGGER.new(log_file)
+  # LOGGER.level = LOGGER::DEBUG
+  # set :LOGGER, LOGGER
 
   # Configuration for Authentication and Authorization layer
-  conf = YAML::load_file("#{settings.root}/config/adapter.yml")
-  set :auth_address, conf['address']
-  set :auth_port, conf['port']
-  set :api_ver, conf['api_ver']
-  set :pub_key_path, conf['public_key_path']
-  set :reg_path, conf['registration_path']
-  set :login_path, conf['login_path']
+  #conf = YAML::load_file("#{settings.root}/config/adapter.yml")
+  #set :auth_address, conf['address']
+  #set :auth_port, conf['port']
+  #set :api_ver, conf['api_ver']
+  #set :pub_key_path, conf['public_key_path']
+  #set :reg_path, conf['registration_path']
+  #set :login_path, conf['login_path']
   # set :authz_path, conf['authorization_path']
-  set :access_token, nil
+  #set :access_token, nil
 
   # log_file = File.new("#{settings.root}/log/#{settings.environment}.log", 'a+')
   # STDOUT.reopen(log_file)
   # STDOUT.sync = true
-  retries = 0
-  code = 503
-  if ENV['SEC_FLAG'] == 'true'
-    while retries <= 6 do
+  #retries = 0
+  #code = 503
+  #if ENV['SEC_FLAG'] == 'true'
+  #  while retries <= 6 do
       # turn keycloak realm pub key into an actual openssl compat pub key
-      LOGGER.debug(component:LOGGED_COMPONENT, operation:'msg', message:"RETRY=#{retries}")
-      code, keycloak_key = get_public_key(settings.auth_address,
-                                          settings.auth_port,
-                                          settings.api_ver,
-                                          settings.pub_key_path)
-      LOGGER.debug(component:LOGGED_COMPONENT, operation:'msg', message:"PUBLIC_KEY_CODE=#{code}")
-      LOGGER.debug(component:LOGGED_COMPONENT, operation:'msg', message:"PUBLIC_KEY_MSG=#{keycloak_key}")
-      if code.to_i == 200
-        keycloak_key, errors = parse_json(keycloak_key)
-        LOGGER.debug(component:LOGGED_COMPONENT, operation:'msg', message:"PUBLIC_KEY=#{keycloak_key['items']['public-key']}")
-        break unless keycloak_key['items']['public-key'].empty?
-      end
-      retries += 1
-      sleep(8)
-    end
-  end
+  #    LOGGER.debug(component:LOGGED_COMPONENT, operation:'msg', message:"RETRY=#{retries}")
+  #    code, keycloak_key = get_public_key(settings.auth_address,
+  #                                        settings.auth_port,
+  #                                        settings.api_ver,
+  #                                        settings.pub_key_path)
+  #    LOGGER.debug(component:LOGGED_COMPONENT, operation:'msg', message:"PUBLIC_KEY_CODE=#{code}")
+  #    LOGGER.debug(component:LOGGED_COMPONENT, operation:'msg', message:"PUBLIC_KEY_MSG=#{keycloak_key}")
+  #    if code.to_i == 200
+  #      keycloak_key, errors = parse_json(keycloak_key)
+  #      LOGGER.debug(component:LOGGED_COMPONENT, operation:'msg', message:"PUBLIC_KEY=#{keycloak_key['items']['public-key']}")
+  #      break unless keycloak_key['items']['public-key'].empty?
+  #    end
+  #    retries += 1
+  #    sleep(8)
+  #  end
+  #end
 
-  if code.to_i == 200
+  #if code.to_i == 200
     # keycloak_key, errors = parse_json(keycloak_key)
     # LOGGER.debug(component:LOGGED_COMPONENT, operation:'msg', message:"PUBLIC_KEY=#{keycloak_key['items']['public-key']}")
-    @s = "-----BEGIN PUBLIC KEY-----\n"
-    @s += keycloak_key['items']['public-key'].scan(/.{1,64}/).join("\n")
-    @s += "\n-----END PUBLIC KEY-----\n"
-    begin
-      @key = OpenSSL::PKey::RSA.new @s
-      set :keycloak_pub_key, @key
-    rescue
-      set :keycloak_pub_key, nil
-    end
-  else
-    set :keycloak_pub_key, nil
-  end
+  #  @s = "-----BEGIN PUBLIC KEY-----\n"
+  #  @s += keycloak_key['items']['public-key'].scan(/.{1,64}/).join("\n")
+  #  @s += "\n-----END PUBLIC KEY-----\n"
+  #  begin
+  #    @key = OpenSSL::PKey::RSA.new @s
+  #    set :keycloak_pub_key, @key
+  #  rescue
+  #    set :keycloak_pub_key, nil
+  #  end
+  #else
+  #  set :keycloak_pub_key, nil
+  #end
 
-  unless settings.keycloak_pub_key.nil?
-    response, r_code = register_service(settings.auth_address, settings.auth_port, settings.api_ver, settings.reg_path)
-    LOGGER.debug(component:LOGGED_COMPONENT, operation:'msg', message:"REG_RESPONSE=#{response} - #{r_code}")
-    if response
-      access_token = login_service(settings.auth_address, settings.auth_port, settings.api_ver, settings.login_path)
-      LOGGER.debug(component:LOGGED_COMPONENT, operation:'msg', message:"ACCESS_TOKEN=#{access_token}")
-      set :access_token, access_token unless access_token.nil?
-    end
-  end
+  #unless settings.keycloak_pub_key.nil?
+  #  response, r_code = register_service(settings.auth_address, settings.auth_port, settings.api_ver, settings.reg_path)
+  #  LOGGER.debug(component:LOGGED_COMPONENT, operation:'msg', message:"REG_RESPONSE=#{response} - #{r_code}")
+  #  if response
+  #    access_token = login_service(settings.auth_address, settings.auth_port, settings.api_ver, settings.login_path)
+  #    LOGGER.debug(component:LOGGED_COMPONENT, operation:'msg', message:"ACCESS_TOKEN=#{access_token}")
+  #    set :access_token, access_token unless access_token.nil?
+  #  end
+  #end
   # STDOUT.sync = false
 end
 
 before do
-  LOGGER.level = LOGGER::DEBUG
+  # LOGGER.level = LOGGER::DEBUG
 
-  log_file = File.new("#{settings.root}/log/#{settings.environment}.log", 'a+')
-  STDOUT.reopen(log_file)
-  STDOUT.sync = true
+  # log_file = File.new("#{settings.root}/log/#{settings.environment}.log", 'a+')
+  # STDOUT.reopen(log_file)
+  # STDOUT.sync = true
 
   # SECURITY CHECKS
-  unless settings.keycloak_pub_key.nil? || settings.access_token.nil?
-    settings.LOGGER.debug "TOKEN_TO_CHECK=#{settings.access_token}"
-    status = decode_token(settings.access_token, settings.keycloak_pub_key)
-    settings.LOGGER.debug "TOKEN_STATUS=#{status}"
-    unless status
-    access_token = login_service(settings.auth_address, settings.auth_port, settings.api_ver, settings.login_path)
-    settings.access_token = access_token unless access_token.nil?
-    end
-  end
-  STDOUT.sync = false
+  # unless settings.keycloak_pub_key.nil? || settings.access_token.nil?
+  #  settings.LOGGER.debug "TOKEN_TO_CHECK=#{settings.access_token}"
+  #  status = decode_token(settings.access_token, settings.keycloak_pub_key)
+  #  settings.LOGGER.debug "TOKEN_STATUS=#{status}"
+  #  unless status
+  #  access_token = login_service(settings.auth_address, settings.auth_port, settings.api_ver, settings.login_path)
+  #  settings.access_token = access_token unless access_token.nil?
+  #  end
+  # end
+  # STDOUT.sync = false
 
 end
 
@@ -158,17 +157,17 @@ unless ENV['MAIN_DB'].nil?
   end
 end
 
-unless ENV['SECOND_DB'].nil?
-  p "SECOND_DB = #{ENV['SECOND_DB']}"
-  config = YAML.load_file('config/mongoid.yml')
-  config['production_secondary'] = {'sessions' =>
-                                       {'default' =>
-                                            {'database' => ENV['SECOND_DB'],
-                                                              'hosts' => [ENV['SECOND_DB_HOST']]}}}
-  File.open('config/mongoid.yml','w') do |conf|
-    conf.write config.to_yaml
-  end
-end
+#unless ENV['SECOND_DB'].nil?
+#  p "SECOND_DB = #{ENV['SECOND_DB']}"
+#  config = YAML.load_file('config/mongoid.yml')
+#  config['production_secondary'] = {'sessions' =>
+#                                       {'default' =>
+#                                            {'database' => ENV['SECOND_DB'],
+#                                                              'hosts' => [ENV['SECOND_DB_HOST']]}}}
+#  File.open('config/mongoid.yml','w') do |conf|
+#    conf.write config.to_yaml
+#  end
+#end
 
 # Configurations for Services Repository
 class SonataNsRepository < Sinatra::Application
@@ -176,9 +175,9 @@ class SonataNsRepository < Sinatra::Application
   # Load configurations
   config_file 'config/config.yml'
   Mongoid.load!('config/mongoid.yml')
-  before {
-    env['rack.LOGGER'] = LOGGER.new "#{settings.root}/log/#{settings.environment}.log"
-  }
+  #before {
+  #  env['rack.LOGGER'] = LOGGER.new "#{settings.root}/log/#{settings.environment}.log"
+  #}
 end
 
 # Configurations for Slice Repository
@@ -187,9 +186,9 @@ class SonataNsiRepository < Sinatra::Application
   # Load configurations
   config_file 'config/config.yml'
   Mongoid.load!('config/mongoid.yml')
-  before {
-    env['rack.LOGGER'] = LOGGER.new "#{settings.root}/log/#{settings.environment}.log"
-  }
+  #before {
+  #  env['rack.LOGGER'] = LOGGER.new "#{settings.root}/log/#{settings.environment}.log"
+  #}
 end
 
 # Configurations for Functions Repository
@@ -198,9 +197,9 @@ class SonataVnfRepository < Sinatra::Application
   # Load configurations
   config_file 'config/config.yml'
   Mongoid.load!('config/mongoid.yml')
-  before {
-     env['rack.LOGGER'] = LOGGER.new "#{settings.root}/log/#{settings.environment}.log"
-  }
+  #before {
+  #   env['rack.LOGGER'] = LOGGER.new "#{settings.root}/log/#{settings.environment}.log"
+  #}
 end
 
 # Configurations for Catalogues
@@ -209,7 +208,7 @@ class TangoVnVTrRepository < Sinatra::Application
   # Load configurations
   config_file 'config/config.yml'
   Mongoid.load!('config/mongoid.yml')
-  before {
-    env['rack.LOGGER'] = LOGGER.new "#{settings.root}/log/#{settings.environment}.log"
-  }
+  #before {
+  #  env['rack.LOGGER'] = LOGGER.new "#{settings.root}/log/#{settings.environment}.log"
+  #}
 end
